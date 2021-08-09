e70622066dd817e9d6821c51ac97458e4d73c333
Ceejay Gabrang <71625955+CEGA22@users.noreply.github.com>
2021-08-09 21:57:38 +0800

    Added LGA Connect Portal Project

---
+++ LGAConnectPortal/LGAConnectPortal/ViewModels/HomeViewModel.cs
@@ -0,0 +1,38 @@
+﻿using LGAConnectPortal.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace LGAConnectPortal.ViewModels
+{
+    public class HomeViewModel
+    {
+        public HomeViewModel()
+        {
+
+        }
+
+        public ICommand _accountCommand => new Command(GotoMenuPage);
+        public ICommand _latestNewsandAnnouncement => new Command(GotoLatestNewsandAnnouncementPage);
+        public ICommand _viewGrades => new Command(GotoViewGrades);
+
+        void GotoMenuPage()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new MenuPage());
+        }
+
+        void GotoLatestNewsandAnnouncementPage()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new LatestNewsandAnnouncementPage());
+        }
+
+        void GotoViewGrades()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new ViewGrades());
+        }
+
+
+    }
+}
---
+++ LGAConnectPortal/LGAConnectPortal/ViewModels/MenuPageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using LGAConnectPortal.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace LGAConnectPortal.ViewModels
+{
+    public class MenuPageViewModel : INotifyPropertyChanged
+    {       
+        public event PropertyChangedEventHandler PropertyChanged;
+       
+        public MenuPageViewModel()
+        {
+            
+            //this.accountcommand = new Command(
+            //    async () =>
+            //    {
+            //        await Application.Current.MainPage.Navigation.PushAsync(new AccountSettingsPage());
+            //    });
+
+        }
+
+        public ICommand _accountSettings => new Command(GotoAccountSettings);
+
+        void GotoAccountSettings()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new AccountSettingsPage());
+        }
+
+     
+
+    }
+}
