11264897bbea6535fd90e9808474b71851fb07d8
Ceejay Gabrang <71625955+CEGA22@users.noreply.github.com>
2021-08-14 15:33:55 +0800

    Added Fonticon, design, and LGA Logo

--- LGAConnectPortal/LGAConnectPortal/ViewModels/LoginPageViewModel.cs
+++ LGAConnectPortal/LGAConnectPortal/ViewModels/LoginPageViewModel.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using LGAConnectPortal.Views;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace LGAConnectPortal.ViewModels
 {
     public class LoginPageViewModel
     {
+        public LoginPageViewModel()
+        {
+
+        }
+
+        public ICommand _HomePage => new Command(GotoHomePage);
+
+        void GotoHomePage()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new DashboardTabbedPage());
+        }
     }
 }
