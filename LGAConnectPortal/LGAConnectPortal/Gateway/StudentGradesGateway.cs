a895a2c19a2ca0e32e6d0479c2930b95552fb04b
Ceejay Gabrang <71625955+CEGA22@users.noreply.github.com>
2021-12-20 10:27:20 +0800

    Added View Grades

---
+++ LGAConnectPortal/LGAConnectPortal/Gateway/StudentGradesGateway.cs
@@ -0,0 +1,31 @@
+﻿using LGAConnectPortal.Helpers;
+using LGAConnectPortal.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LGAConnectPortal.Gateway
+{
+    public class StudentGradesGateway
+    {
+        static string BaseUrl = "http://ceejaygabrang-001-site1.itempurl.com/api/lga/classrecordstudent";
+
+        public async Task<IEnumerable<StudentGrades>> GetStudentGradesByID(int ID)
+        {
+            try
+            {
+                string url = $"{BaseUrl}/get_all/{ID}";
+                var content = await WebMethods.MakeGetRequest(url);
+                var result = JsonConvert.DeserializeObject<IEnumerable<StudentGrades>>(content);
+                return result;
+            }
+            catch
+            {
+                return Enumerable.Empty<StudentGrades>();
+            }
+        }
+    }
+}
--- LGAConnectPortal/LGAConnectPortal/ViewModels/ViewGradesViewModel.cs
+++ LGAConnectPortal/LGAConnectPortal/ViewModels/ViewGradesViewModel.cs
@@ -1,10 +1,65 @@
-﻿using System;
+﻿using LGAConnectPortal.Models;
+using LGAConnectPortal.Services;
+using MvvmHelpers;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 namespace LGAConnectPortal.ViewModels
 {
-    public class ViewGradesViewModel
+    public class ViewGradesViewModel : BaseViewModel
     {
+
+        //public ObservableRangeCollection<StudentGrades> studentGrades { get; }
+
+        public List<StudentGradesPerSubject> StudentGrades { get; set; } = new List<StudentGradesPerSubject>();
+
+        public ViewGradesViewModel()
+        {
+            //studentGrades = new ObservableRangeCollection<StudentGrades>();
+            PrepareBindings();
+        }
+
+        public async void PrepareBindings()
+        {
+            await DisplayStudentGrades();
+        }
+
+        IEnumerable<StudentGrades> studentGradesList = new List<StudentGrades>();
+        public async Task DisplayStudentGrades()
+        {
+            var ID = Preferences.Get("ID", 0);           
+            studentGradesList = await StudentGradesService.GetStudentgradesByID(ID);                 
+            //studentGrades.AddRange(studentGradesList);
+
+            var subjectList = studentGradesList.Select(x => x.SubjectName).Distinct();
+
+            foreach(var subject in subjectList) 
+            {
+                var quarterGrades = studentGradesList.Where(x => x.SubjectName == subject).OrderBy(o => o.GradingPeriod);
+
+                if (quarterGrades.Any())
+                {
+                    var firstGrading = quarterGrades.FirstOrDefault(x => x.GradingPeriod == 1).QuarterlyGrade;
+                    var secondGrading = quarterGrades.FirstOrDefault(x => x.GradingPeriod == 2).QuarterlyGrade;
+                    var thirdGrading = quarterGrades.FirstOrDefault(x => x.GradingPeriod == 3).QuarterlyGrade;
+                    var fourthGrading = quarterGrades.FirstOrDefault(x => x.GradingPeriod == 4).QuarterlyGrade;
+
+                    var itemToAdd = new StudentGradesPerSubject
+                    {
+                        SubjectName = subject,
+                        FirstGrading = firstGrading,
+                        SecondGrading = secondGrading,
+                        ThirdGrading = thirdGrading,
+                        FourthGrading = fourthGrading
+                    };
+
+                    StudentGrades.Add(itemToAdd);
+                }           
+            }
+        }
     }
 }
